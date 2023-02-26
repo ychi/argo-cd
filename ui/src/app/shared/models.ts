f62ae7b1c4f01777cb7bb56e582b1fefd44fa7c8
Nicholas Morey <nicholas@morey.tech>
2023-02-26 21:41:56 +0000

    sibling of d3ff9757c460ae1a6a11e1231251b5d27aadcdd1

--- ui/src/app/shared/models.ts
+++ ui/src/app/shared/models.ts
@@ -123,8 +123,6 @@ export interface ResourceResult {
 export const AnnotationRefreshKey = 'argocd.argoproj.io/refresh';
 export const AnnotationHookKey = 'argocd.argoproj.io/hook';
 export const AnnotationSyncWaveKey = 'argocd.argoproj.io/sync-wave';
-export const AnnotationDefaultView = 'pref.argocd.argoproj.io/default-view';
-export const AnnotationDefaultPodSort = 'pref.argocd.argoproj.io/default-pod-sort';
 
 export interface Application {
     apiVersion?: string;
@@ -133,7 +131,6 @@ export interface Application {
     spec: ApplicationSpec;
     status: ApplicationStatus;
     operation?: Operation;
-    isAppOfAppsPattern?: boolean;
 }
 
 export type WatchType = 'ADDED' | 'MODIFIED' | 'DELETED' | 'ERROR';
@@ -214,14 +211,6 @@ export interface EnvEntry {
 export interface ApplicationSourcePlugin {
     name: string;
     env: EnvEntry[];
-    parameters?: Parameter[];
-}
-
-export interface Parameter {
-    name: string;
-    string?: string;
-    array?: string[];
-    map?: Map<string, string>;
 }
 
 export interface JsonnetVar {
@@ -238,8 +227,6 @@ interface ApplicationSourceJsonnet {
 export interface ApplicationSourceDirectory {
     recurse: boolean;
     jsonnet?: ApplicationSourceJsonnet;
-    include?: string;
-    exclude?: string;
 }
 
 export interface Automated {
@@ -261,7 +248,6 @@ export interface Info {
 export interface ApplicationSpec {
     project: string;
     source: ApplicationSource;
-    sources: ApplicationSource[];
     destination: ApplicationDestination;
     syncPolicy?: SyncPolicy;
     ignoreDifferences?: ResourceIgnoreDifferences[];
@@ -285,8 +271,6 @@ export interface RevisionHistory {
     id: number;
     revision: string;
     source: ApplicationSource;
-    revisions: string[];
-    sources: ApplicationSource[];
     deployStartedAt: models.Time;
     deployedAt: models.Time;
 }
@@ -325,10 +309,9 @@ export interface ResourceStatus {
     name: string;
     status: SyncStatusCode;
     health: HealthStatus;
-    createdAt?: models.Time;
     hook?: boolean;
     requiresPruning?: boolean;
-    syncWave?: number;
+    syncOrder?: string;
 }
 
 export interface ResourceRef {
@@ -392,7 +375,6 @@ export interface SyncStatus {
     comparedTo: ApplicationSource;
     status: SyncStatusCode;
     revision: string;
-    revisions: string[];
 }
 
 export interface ApplicationCondition {
@@ -461,6 +443,7 @@ export interface AuthSettings {
         chatText: string;
         binaryUrls: Record<string, string>;
     };
+    plugins: Plugin[];
     userLoginsDisabled: boolean;
     kustomizeVersions: string[];
     uiCssURL: string;
@@ -469,7 +452,6 @@ export interface AuthSettings {
     uiBannerPermanent: boolean;
     uiBannerPosition: string;
     execEnabled: boolean;
-    appsInAnyNamespaceEnabled: boolean;
 }
 
 export interface UserInfo {
@@ -508,16 +490,6 @@ export interface Repository {
     type?: string;
     name?: string;
     connectionState: ConnectionState;
-    project?: string;
-    username?: string;
-    password?: string;
-    tlsClientCertData?: string;
-    tlsClientCertKey?: string;
-    proxy?: string;
-    insecure?: boolean;
-    enableLfs?: boolean;
-    githubAppId?: string;
-    forceHttpBasicAuth?: boolean;
 }
 
 export interface RepositoryList extends ItemsList<Repository> {}
@@ -548,8 +520,6 @@ export interface Cluster {
         connectionState: ConnectionState;
         cacheInfo: ClusterCacheInfo;
     };
-    annotations?: {[name: string]: string};
-    labels?: {[name: string]: string};
 }
 
 export interface ClusterCacheInfo {
@@ -613,19 +583,6 @@ export interface KustomizeAppSpec {
 export interface PluginAppSpec {
     name: string;
     env: EnvEntry[];
-    parametersAnnouncement?: ParameterAnnouncement[];
-}
-
-export interface ParameterAnnouncement {
-    name?: string;
-    title?: string;
-    tooltip?: string;
-    required?: boolean;
-    itemType?: string;
-    collectionType?: string;
-    string?: string;
-    array?: string[];
-    map?: Map<string, string>;
 }
 
 export interface ObjectReference {
@@ -922,19 +879,4 @@ export enum PodPhase {
     PodSucceeded = 'Succeeded',
     PodFailed = 'Failed',
     PodUnknown = 'Unknown'
-}
-
-export interface NotificationChunk {
-    name: string;
-}
-
-export interface LinkInfo {
-    title: string;
-    url: string;
-    description?: string;
-    iconClass?: string;
-}
-
-export interface LinksResponse {
-    items: LinkInfo[];
 }
